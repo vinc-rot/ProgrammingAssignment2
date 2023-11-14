4aa0ac6a7618b70def9bd34a9ade64221925a370
vinc-rot <vrhack1@gmail.com>
2023-11-14 11:38:28 +0100

    Solving tasks

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,54 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a cache for a matrix, allowing the storage
+## of both the original matrix and its inverse to improve efficiency
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Initialize a variable to store the inverse matrix
+  inverse <- NULL
+  
+  # Function to set the matrix
+  setMatrix <- function(newMatrix) {
+    x <<- newMatrix
+    # Reset the cached inverse when the matrix is updated
+    inverse <<- NULL
+  }
+  
+  # Function to get the matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  # Function to set the inverse matrix to the cache
+  setInverse <- function(newInverse) {
+    inverse <<- newInverse
+  }
+  
+  # Function to get the inverse matrix from the cache
+  getInverse <- function() {
+    inverse
+  }
+  
+  # Return a list of functions for manipulating the matrix and its inverse
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computes and returns the inverse of a matrix using a cache
+## If the inverse has already been calculated and stored in the cache, it is returned directly
+cacheSolve <- function(cacheMatrix, ...) {
+  # Retrieve the matrix and its inverse from the cache
+  matrix <- cacheMatrix$getMatrix()
+  inverse <- cacheMatrix$getInverse()
+  
+  # If the inverse is not cached or the matrix has changed, compute the inverse
+  if (is.null(inverse) || !identical(matrix, cacheMatrix$getMatrix())) {
+    inverse <- solve(matrix, ...)
+    # Cache the newly computed inverse
+    cacheMatrix$setInverse(inverse)
+  }
+  
+  # Return the cached inverse
+  inverse
 }
